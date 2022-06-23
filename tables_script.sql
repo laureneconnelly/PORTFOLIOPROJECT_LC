30a22d8c42139c69d491506f64112eed1ea16f84
Laurene Connelly <laureen.connelly@gmail.com>
2022-06-22 19:18:20 -0700

    package commit

---
+++ tables_script.sql
@@ -0,0 +1,61 @@
+--songs list
+
+/* 
+INSERT INTO songs(title, year, artist_id, album_id)
+VALUES ('Wannabe', 1996, 3, 5),
+('Say You''ll Be There', 1996, 3, 5),
+('2 Become 1', 1996, 3, 5),
+('Love Thing', 1996, 3, 5),
+('Last Time Lover', 1996, 3, 5),
+('Mama', 1996, 3, 5),
+('Who Do You Think You Are', 1996, 3, 5),
+('Something Kinda Funny', 1996, 3, 5),
+('Naked', 1996, 3, 5),
+('If U Can''t Dance', 1996, 3, 5);
+
+INSERT INTO songs(title, year, artist_id, album_id)
+VALUES ('Smoke Signals', 2017, 2, 6),
+('Motion Sickness', 2017, 2, 6),
+('Funeral', 2017, 2, 6),
+('Demi Moore', 2017, 2, 6),
+('Scott Street', 2017, 2, 6),
+('Killer', 2017, 2, 6),
+('Georgia', 2017, 2, 6),
+('Chelsea', 2017, 2, 6),
+('Would You Rather', 2017, 2, 6),
+('You Missed My Heart', 2017, 2, 6),
+('Smoke Signals (Reprise)', 2017, 2, 6);
+
+INSERT INTO songs(title, year, artist_id, album_id)
+VALUES ('Woman', 2021, 1, 7),
+('Naked', 2021, 1, 7),
+('Payday (feat. Young Thug)', 2021, 1, 7),
+('Get Into It (Yuh)', 2021, 1, 7),
+('Need to Know', 2021, 1, 7),
+('I Don''t Do Drugs (feat. Ariana Grande)', 2021, 1, 7),
+('Love To Dream', 2021, 1, 7),
+('You Right', 2021, 1, 7),
+('Been Like This', 2021, 1, 7),
+('Options (feat. JID)', 2021, 1, 7),
+('Ain''t Shit', 2021, 1, 7),
+('Imagine', 2021, 1, 7),
+('Alone', 2021, 1, 7),
+('Kiss Me More (feat. SZA)', 2021, 1, 7);  */
+
+
+--artist list
+/* INSERT INTO artists (name, genre)
+VALUES 
+('Doja Cat', 'hip hop pop'),
+('Phoebe Bridgers', 'indie'),
+('Spice Girls', 'pop'); */
+
+
+
+--albums list
+/*insert into albums (title, year, genre, artist_id)
+values
+('Spice', 1996, 'pop', 3),
+('Stranger in the Alps', 2017, 'indie', 2),
+('Planet Her', 2021, 'hip hop pop', 1),
+('Spice World', 1997, 'pop', 3); */
